4237cd9b0e7d5ab9c1e686eb793a008b42da6c14
Gareth Carless <garethcarless@btinternet.com>
2016-09-23 11:35:46 +0100

    Initial setup, view controllers created with table views added to the meals and lists views

---
+++ ShoppingLists/ListsViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ListsViewController.swift
+//  ShoppingLists
+//
+//  Created by Gareth Carless on 23/09/2016.
+//  Copyright © 2016 Gareth Carless. All rights reserved.
+//
+
+import UIKit
+
+class ListsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ ShoppingLists/SettingsViewController.swift
@@ -0,0 +1,24 @@
+//
+//  SettingsViewController.swift
+//  ShoppingLists
+//
+//  Created by Gareth Carless on 23/09/2016.
+//  Copyright © 2016 Gareth Carless. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
