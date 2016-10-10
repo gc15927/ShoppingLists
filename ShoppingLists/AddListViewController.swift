e145db85729a7c39355662fc24529eab793a17e2
Gareth Carless <garethcarless@btinternet.com>
2016-10-10 11:38:26 +0100

    continued ux for creating a shopping list; picker view appears to add a meal to the list. functionality needed here next

--- ShoppingLists/AddListViewController.swift
+++ ShoppingLists/AddListViewController.swift
@@ -8,12 +8,34 @@
 
 import UIKit
 
-class AddListViewController: UIViewController {
-
+class AddListViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    @IBOutlet weak var saveButton: UIBarButtonItem!
+    @IBOutlet weak var itemsTable: UITableView!
+    @IBOutlet weak var mealsTable: UITableView!
+    @IBOutlet weak var mealsPicker: UIPickerView!
+    
+    var meals = [Meal]()
+    var items = [Ingredient]()
+    var existingMeals = [Meal]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        //Load meals from memory, fill table view with them
+        let fileManager = NSFileManager.defaultManager()
+        if !fileManager.fileExistsAtPath(Meal.ArchiveURL.path!) {
+            //No meals saved
+        }
+        else {
+            //Add new meal to list of meals
+            existingMeals = NSKeyedUnarchiver.unarchiveObjectWithFile(Meal.ArchiveURL.path!) as! [Meal]
+        }
+        mealsPicker.hidden = true
+        mealsPicker.delegate = self
+        mealsPicker.dataSource = self
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -24,13 +46,99 @@ class AddListViewController: UIViewController {
     //Respond to user pressing the cancel button in the navigation bar, only operation
     //needed is to close the view, as no meal data is saved until the done button is pressed
     @IBAction func cancelButtonPressed(sender: AnyObject) {
-        self.dismissViewControllerAnimated(true, completion: nil)
+        if(saveButton.title == "Save") {
+            //Close add list view
+            self.dismissViewControllerAnimated(true, completion: nil)
+        }
+        else {
+            //Close mealPicker
+            saveButton.title = "Save"
+            mealsPicker.hidden = true
+        }
     }
     
     //Respond to user pressing the done button in the navigation bar, generate Meal object
     //and send it to the parent view controller to be added to the list of meals. Also close view
-    @IBAction func doneButtonPressed(sender: AnyObject) {
-        self.dismissViewControllerAnimated(true, completion: nil)
+    @IBAction func saveButtonPressed(sender: AnyObject) {
+        if(saveButton.title == "Save") {
+            //Save list
+            self.dismissViewControllerAnimated(true, completion: nil)
+        }
+        else {
+            //Add meal to list of meals
+            saveButton.title = "Save"
+            mealsPicker.hidden = true
+        }
+    }
+    
+    @IBAction func addMeal(sender: AnyObject) {
+        mealsPicker.hidden = false
+        saveButton.title = "Add"
     }
 
+    @IBAction func addItem(sender: AnyObject) {
+    
+    }
+    
+    //PICKER
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 2
+    }
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        if(component == 0) {
+            return existingMeals.count + 1
+        }
+        else {
+            return 13
+        }
+    }
+    
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        if(component == 0) {
+            if(row == 0) {
+                return "Meal"
+            }
+            else {
+                return existingMeals[row-1].getName()
+            }
+        }
+        else {
+            if(row == 0) {
+                return "People"
+            }
+            else {
+                return String(row)
+            }
+        }
+    }
+    
+    
+    //TABLE
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        switch(tableView.restorationIdentifier!) {
+            case "meals":
+                return meals.count
+            case "items":
+                return items.count
+            default:
+                break
+        }
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cellIdentifier = "MealTableViewCell"
+        let cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier, forIndexPath: indexPath) as! MealTableViewCell
+        
+        // Configure the cell...
+        
+        return cell
+    }
+    
 }
